7f91fd38d880ef0a88813b1c30e70756940d27d2
SasaDjordjevic <51803563+SasaDjordjevic@users.noreply.github.com>
2020-06-15 12:42:23 +0200

    read more update

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -20,56 +20,47 @@ window.sr = ScrollReveal();
   sr.reveal('.text1', {
     duration: 2000,
     origin: 'top',
-    distance: "50px",
-    reset:true
+    distance: "50px"
   });
   sr.reveal('#percenages, #education', {
     duration: 2000,
     origin: 'bottom',
-    distance: "50px",
-    reset:true
+    distance: "50px"
   });
   sr.reveal('#projectText1', {
     duration: 2000,
     origin: 'top',
-    distance: "30px",
-    reset:true
+    distance: "30px"
   });
   sr.reveal('#project1', {
     duration: 2000,
     origin: 'bottom',
-    distance: "30px",
-    reset:true
+    distance: "30px"
   });
   sr.reveal('#projectText2', {
     duration: 2000,
     origin: 'bottom',
-    distance: "30px",
-    reset:true
+    distance: "30px"
   });
   sr.reveal('#project2', {
     duration: 2000,
     origin: 'top',
-    distance: "30px",
-    reset:true
+    distance: "30px"
   });
   sr.reveal('.skillText', {
     duration: 2000,
     origin: 'top',
-    distance: "30px",
-    reset:true
+    distance: "30px"
   });
   sr.reveal('.imgHolder', {
     duration: 2000,
     origin: 'bottom',
-    distance: "30px",
-    reset:true
+    distance: "30px"
   });
   sr.reveal('#informations', {
     duration: 2000,
     origin: 'top',
-    distance: "30px",
-    reset:true
+    distance: "30px"
   });
 
 
